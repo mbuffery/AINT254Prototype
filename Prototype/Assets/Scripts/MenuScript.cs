9c147040d6810bfd5910bc5b5ea8c41813d1ec84
Miles Buffery <miles.buffery@students.plymouth.ac.uk>
2017-01-13 00:03:43 +0000

    Added more content on start screen and created more art

---
+++ Prototype/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+    public Canvas MainCanvas;
+    public Canvas ControlsCanvas;
+    public Canvas HowToPlayCanvas;
+    
+   
+
+    void Awake()
+    {
+        
+        ControlsCanvas.enabled = false;
+        HowToPlayCanvas.enabled = false;
+        
+        
+    }
+    public void ReturnOn()
+    {
+        
+        MainCanvas.enabled = true;
+        ControlsCanvas.enabled = false;
+        HowToPlayCanvas.enabled = false;
+        
+        
+    }
+    public void ControlsOn()
+    {
+        
+        MainCanvas.enabled = false;
+        ControlsCanvas.enabled = true;
+        HowToPlayCanvas.enabled = false;
+        
+        
+    }
+    public void HelpOn()
+    {
+       
+        MainCanvas.enabled = false;
+        ControlsCanvas.enabled = false;
+        HowToPlayCanvas.enabled = true;
+        
+        
+    }
+    
+    public void LoadOn()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
